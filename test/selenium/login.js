bb60f80e82d7fe8704a20c3d5a476911f0eafc82
Tanmay Chandekar <tanmaychandekar98@gmail.com>
2018-04-27 22:15:52 +0530

    test-report with jasmine-js

--- test/selenium/login.js
+++ test/selenium/login.js
@@ -5,31 +5,23 @@ var webdriver = require('selenium-webdriver'),
 var driver = new webdriver.Builder().
     forBrowser('chrome').
     build();
-
-
-driver.get('http://localhost:3000/signup/admin').then(function(){
-    driver.findElement(By.className('cname')).sendKeys('QQQQQ');
-    driver.findElement(By.className('id')).sendKeys('5006');
-    driver.findElement(By.className('name')).sendKeys('Robert');
-    driver.findElement(By.className('password')).sendKeys('admin');   
+var username = ['a1', 'a2'];
+var password = ['p1', 'p1'];
+for(var i=0;i<2;i++){
+driver.get('http://localhost:3000').then(function(){
+    driver.findElement(By.className('username')).sendKeys(username[i]);
+    driver.findElement(By.className('password')).sendKeys(password[i]);   
 
     driver.wait(function(){
         return driver.findElement(By.className('password')).getAttribute('value');
 }, 10000).then(function(value){ console.log(value)});
 
 //console.info( await driver.manage().getTimeouts() );
-driver.findElement(By.className('register')).click();
-
-driver.wait(function(){
-    return driver.findElement(By.className('username'));
-}, 10000).then(function(){
-
-    driver.findElement(By.className('username')).sendKeys('5001');
-});
+driver.findElement(By.className('submit')).click();
 
 
 });
-
+}
 /*driver.get('http://localhost:3000')
 driver.findElement(By.className('username')).sendKeys('3')
 driver.findElement(By.className('password')).sendKeys('abc3pqr');
